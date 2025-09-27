fb18e144ea8a445e865a2216cffab6c451213820
pops <pops@DESKTOP-P3QPI08>
2025-09-27 20:05:11 +0300

    Добавьте файлы проекта.

---
+++ PasteToOldPaintTemplate/Pixel.cs
@@ -0,0 +1,9 @@
+﻿namespace PasteToOldPaintTemplate
+{
+    public struct Pixel
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+    }
+}
---
+++ PasteToOldPaintTemplate/Program.cs
@@ -0,0 +1,281 @@
+﻿using System.Drawing;
+
+namespace PasteToOldPaintTemplate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1 || !Path.Exists(args[0]) || !File.Exists(args[0]))
+            {
+                Console.WriteLine("Path to file not found");
+                Console.ReadLine();
+                return;
+            }
+
+            var path = args[0];
+
+            Console.WriteLine("Input pixel size:");
+            if (!int.TryParse(Console.ReadLine(), out int pixelSize) || pixelSize <= 0)
+            {
+                Console.WriteLine("Invalid input pixel size");
+                Console.ReadLine();
+                return;
+            }
+
+            var bitmap = new Bitmap(path);
+            bitmap = CreatePixelImg(bitmap, pixelSize);
+
+            var pixels = GetPixelsFromBitmap(bitmap);
+            var largePixelColor = GetLargePixelColor(pixels);
+            var pallete = pixels.ToHashSet().ToList();
+
+            bitmap = ResizeImageWithBackground(bitmap, TemplateConst.mainAreaWidth, TemplateConst.mainAreaHeight,
+                Color.FromArgb(255, largePixelColor.R, largePixelColor.G, largePixelColor.B));
+
+            var a = new MemoryStream(Convert.FromBase64String(TemplateConst.template));
+            var bitmapWithTemplate = PasteImgInTemplate(bitmap, new Bitmap(a), TemplateConst.mainAreaX, TemplateConst.mainAreaY);
+            bitmapWithTemplate = PastePallete(bitmapWithTemplate, pallete);
+            var savePath = $"{path.Substring(0, path.LastIndexOf('.'))}_PintPixel_ps{pixelSize}{path.Substring(path.LastIndexOf('.'))}";
+            bitmapWithTemplate.Save(savePath);
+        }
+
+        public static Bitmap PastePallete(Bitmap bitmap, List<Pixel> pallete)
+        {
+            for (int i = 0; i < 28 - pallete.Count; i++)
+            {
+                pallete.Add(new Pixel());
+            }
+
+            for (int i = 0; i < pallete.Count; i++)
+            {
+                var onePallete = new Bitmap(TemplateConst.palleteSize, TemplateConst.palleteSize);
+                for (int h = 0; h < onePallete.Height; h++)
+                    for (int w = 0; w < onePallete.Width; w++)
+                        onePallete.SetPixel(h, w, Color.FromArgb(255, pallete[i].R, pallete[i].G, pallete[i].B));
+
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    var x = TemplateConst.palleteX + (i % 14 * TemplateConst.palleteSize) + (i % 14 * 3);
+                    var y = TemplateConst.palleteY + (i >= 14 ? TemplateConst.palleteSize + 3 : 0);
+
+                    graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                    graphics.DrawImage(onePallete, x, y);
+                }
+            }
+
+            return bitmap;
+        }
+
+        public static List<Pixel> GetPixelsFromBitmap(Bitmap bitmap)
+        {
+            var pixels = new List<Pixel>();
+
+            for (int y = 0; y < bitmap.Height; y += 1)
+            {
+                for (int x = 0; x < bitmap.Width; x += 1)
+                {
+                    pixels.Add(new Pixel()
+                    {
+                        R = bitmap.GetPixel(x, y).R,
+                        G = bitmap.GetPixel(x, y).G,
+                        B = bitmap.GetPixel(x, y).B,
+                    });
+                }
+            }
+
+            return pixels;
+        }
+
+        public static Pixel GetLargePixelColor(List<Pixel> pixels)
+        {
+            return pixels.GroupBy(p => new Pixel() { R = p.R, G = p.G, B = p.B })
+                                    .OrderByDescending(g => g.Count())
+                                    .First().Key;
+        }
+
+        public static Bitmap CreatePixelImg(Bitmap bitmap, int pixelSize)
+        {
+            var newWidth = (int)Math.Ceiling((double)bitmap.Width / pixelSize);
+            var newHeight = (int)Math.Ceiling((double)bitmap.Height / pixelSize);
+
+            var pixels = new List<Pixel>();
+
+            for (int y = 0; y < bitmap.Height; y += pixelSize)
+            {
+                for (int x = 0; x < bitmap.Width; x += pixelSize)
+                {
+                    int blockWidth = Math.Min(pixelSize, bitmap.Width - x);
+                    int blockHeight = Math.Min(pixelSize, bitmap.Height - y);
+
+                    pixels.Add(GetAvgRGBCanal(bitmap, x, y, blockWidth, blockHeight));
+                }
+            }
+            pixels = ReduceColorsKMeans(pixels.ToArray(), 28).ToList();
+            var largePixelColor = GetLargePixelColor(pixels);
+            var newBitmap = new Bitmap(newWidth, newHeight);
+
+            for (int i = 0; i < pixels.Count; i++)
+            {
+                int x = i % newWidth;
+                int y = i / newWidth;
+
+                if (y < newHeight)
+                {
+                    var color = Color.FromArgb(255, pixels[i].R, pixels[i].G, pixels[i].B);
+                    newBitmap.SetPixel(x, y, color);
+                }
+            }
+            return newBitmap;
+        }
+
+        public static Bitmap PasteImgInTemplate(Image originalImage, Image TemplateImage, int pasteX, int pasteY)
+        {
+            var newBitmap = new Bitmap(TemplateImage);
+
+            using (var graphics = Graphics.FromImage(newBitmap))
+            {
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                graphics.DrawImage(originalImage, pasteX, pasteY);
+            }
+
+            return newBitmap;
+        }
+
+        public static Bitmap ResizeImageWithBackground(Image originalImage, int targetWidth, int targetHeight, Color backgroundColor)
+        {
+            double ratioX = (double)targetWidth / originalImage.Width;
+            double ratioY = (double)targetHeight / originalImage.Height;
+            double ratio = Math.Min(ratioX, ratioY);
+
+            int newWidth = (int)(originalImage.Width * ratio);
+            int newHeight = (int)(originalImage.Height * ratio);
+
+            var newBitmap = new Bitmap(targetWidth, targetHeight);
+
+            using (var graphics = Graphics.FromImage(newBitmap))
+            {
+                graphics.Clear(backgroundColor);
+                int x = (targetWidth - newWidth) / 2;
+                int y = (targetHeight - newHeight) / 2;
+
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                graphics.DrawImage(originalImage, x, y, newWidth, newHeight);
+            }
+
+            return newBitmap;
+        }
+
+        public static Pixel[] ReduceColorsKMeans(Pixel[] pixels, int targetColors)
+        {
+            if (pixels.Length == 0 || targetColors <= 0) return pixels;
+            var points = pixels.Select(p => new double[] { p.R, p.G, p.B }).ToArray();
+            var random = new Random();
+            var centroids = new List<double[]>();
+            for (int i = 0; i < targetColors; i++)
+            {
+                var randomPixel = points[random.Next(points.Length)];
+                centroids.Add(new double[] { randomPixel[0], randomPixel[1], randomPixel[2] });
+            }
+            for (int iteration = 0; iteration < 10; iteration++)
+            {
+                var clusters = new List<int>[targetColors];
+                for (int i = 0; i < targetColors; i++)
+                    clusters[i] = new List<int>();
+
+                for (int i = 0; i < points.Length; i++)
+                {
+                    int closestCentroid = 0;
+                    double minDistance = double.MaxValue;
+
+                    for (int j = 0; j < centroids.Count; j++)
+                    {
+                        double distance = Math.Sqrt(
+                            Math.Pow(points[i][0] - centroids[j][0], 2) +
+                            Math.Pow(points[i][1] - centroids[j][1], 2) +
+                            Math.Pow(points[i][2] - centroids[j][2], 2));
+
+                        if (distance < minDistance)
+                        {
+                            minDistance = distance;
+                            closestCentroid = j;
+                        }
+                    }
+                    clusters[closestCentroid].Add(i);
+                }
+
+                for (int i = 0; i < targetColors; i++)
+                {
+                    if (clusters[i].Count > 0)
+                    {
+                        double avgR = clusters[i].Average(idx => points[idx][0]);
+                        double avgG = clusters[i].Average(idx => points[idx][1]);
+                        double avgB = clusters[i].Average(idx => points[idx][2]);
+
+                        centroids[i] = new double[] { avgR, avgG, avgB };
+                    }
+                }
+            }
+
+            Pixel[] palette = new Pixel[centroids.Count];
+            for (int i = 0; i < centroids.Count; i++)
+            {
+                palette[i] = new Pixel
+                {
+                    R = (byte)Math.Round(centroids[i][0]),
+                    G = (byte)Math.Round(centroids[i][1]),
+                    B = (byte)Math.Round(centroids[i][2])
+                };
+            }
+
+            Pixel[] result = new Pixel[pixels.Length];
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                int closestColorIndex = 0;
+                double minDistance = double.MaxValue;
+
+                for (int j = 0; j < palette.Length; j++)
+                {
+                    double distance = Math.Sqrt(
+                        Math.Pow(pixels[i].R - palette[j].R, 2) +
+                        Math.Pow(pixels[i].G - palette[j].G, 2) +
+                        Math.Pow(pixels[i].B - palette[j].B, 2));
+
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestColorIndex = j;
+                    }
+                }
+
+                result[i] = palette[closestColorIndex];
+            }
+
+            return result;
+        }
+
+        public static Pixel GetAvgRGBCanal(Bitmap bitmap, int x, int y, int blockWidth, int blockHeight)
+        {
+            int[] rgb = new int[3] { 0, 0, 0 };
+            int pixelCount = 0;
+
+            for (int i = 0; i < blockHeight; i++)
+            {
+                for (int k = 0; k < blockWidth; k++)
+                {
+                    var pixel = bitmap.GetPixel(x + k, y + i);
+                    rgb[0] += pixel.R;
+                    rgb[1] += pixel.G;
+                    rgb[2] += pixel.B;
+                    pixelCount++;
+                }
+            }
+            return new Pixel
+            {
+                R = (byte)(rgb[0] / pixelCount),
+                G = (byte)(rgb[1] / pixelCount),
+                B = (byte)(rgb[2] / pixelCount),
+            };
+        }
+    }
+}
